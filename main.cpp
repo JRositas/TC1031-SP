9ac59f2d3755f28e95a8e348a728992c80d308c7
JRositas <a00829760@itesm.mx>
2021-09-06 12:42:36 -0500

    date2Int function

--- main.cpp
+++ main.cpp
@@ -11,6 +11,59 @@
 
 using namespace std;
 
+void date2Int(Entrada entrada){
+    string date = entrada.getFecha();
+    int dateCode = 0;
+    int aux = 0;
+    int i = 0;
+    while(date[i] != '-'){
+        aux *= 10;
+        aux += (date[i++] - '0');
+    }
+    dateCode += aux;
+
+    string month = "";
+    while(date[++i] != '-'){
+        month += date[i];
+    }
+    int monthCode = 0;
+    if(month == "jan"){
+        monthCode = 1;
+    } else if(month == "feb"){
+        monthCode = 2;
+    } else if(month == "mar"){
+        monthCode = 3;
+    } else if(month == "apr"){
+        monthCode = 4;
+    } else if(month == "may"){
+        monthCode = 5;
+    } else if(month == "jun"){
+        monthCode = 6;
+    } else if(month == "jul"){
+        monthCode = 7;
+    } else if(month == "aug"){
+        monthCode = 8;
+    } else if(month == "sep"){
+        monthCode = 9;
+    } else if(month == "oct"){
+        monthCode = 10;
+    } else if(month == "nov"){
+        monthCode = 11;
+    } else if(month == "dec"){
+        monthCode = 12;
+    }
+    dateCode += (monthCode * 100);
+
+    aux = 0;
+    while(++i < date.length()){
+        aux *= 10;
+        aux += (date[i] - '0');
+    }
+    dateCode += (aux * 10000);
+
+    entrada.setFechaCode(dateCode);
+}
+
 int main(){
     Entrada entrada;
     Entrada* listaEntradas[50];
