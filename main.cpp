cef761a40e15cbb41391b0cbf598c1a23a2c79c5
JRositas <A00829760@itesm.mx>
2021-09-06 10:13:42 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Equipo 12
+// SP TC1031
+// 06 Sep 2021
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+#include "Entrada.h"
+
+using namespace std;
+
+int main(){
+
+}
