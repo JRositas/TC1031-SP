ec4dbc40a8c5de70a0a55e3d3172fab804108e17
JRositas <A00829760@itesm.mx>
2021-09-12 14:42:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -141,7 +141,7 @@ int main(){
     finBuscado = upper_bound(listaPaises.begin(), listaPaises.end(), paisABuscar);
 
     //Despliega los datos de las entradas del país buscado
-    for(int i = (inicioBuscado- listaPaises.begin()); i < (finBuscado- listaPaises.begin()) ; i++){
+    for(int i = ((finBuscado- listaPaises.begin())-1); i > (inicioBuscado- listaPaises.begin()) ; i--){
        mivect[i].mostrar();
     }
   
